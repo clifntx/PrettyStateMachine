68251cb16de4f483d656ec9ce9d147672cacaddd
Clif Boyd <cboyd@allaccessinfotech.com>
2018-08-23 12:34:24 -0400

    Added wks maintenance script.

--- ps/maintenance_wks.ps1
+++ ps/maintenance_wks.ps1
@@ -1,6 +1,6 @@
 ﻿param(
     [string]$configCsv = "",
-    $logLevel = 1
+    $logLevel = -1
     )
 
 function log ($str, $fc="white"){
@@ -11,13 +11,20 @@ function log ($str, $fc="white"){
         "yellow"   {$priority = 4}
         "green"    {$priority = 2} 
         "white"    {$priority = 1}
-        "gray"     {$priority = 0; $str = "  "+$str;}
-        "darkgray" {$priority = -1}
+        "gray"     {$priority = 0; $str = "  ..."+$str;}
+        "darkgray" {$priority = -1; $str = "  >> "+$str;}
         default {$priority = 1; $fc = "white"}
         }
     if ($priority -ge $logLevel) {
         write-host $str -ForegroundColor $fc
     }
+}
+function sum($list){
+    $sum = 0
+    $list | foreach {
+        $sum += $_   
+    }
+    return $sum
 }
 function download($driverUrl, $downloadPath) {
     log " Calling download(`n>>> -driverUrl $driverUrl`n>>> -downloadPath $downloadPath`n>>> )" "darkgray"
@@ -39,10 +46,12 @@ function download($driverUrl, $downloadPath) {
         log ">> UNCAUGHT ERROR: $PSItem" "red"
         log ">> UNCAUGHT ERROR: $($Error[0].Exception.GetType().fullname)" "red"
         return $false
-        }
+    }
     return (Test-Path $downloadPath)
 }
 function buildConfig($configUrl){
+    log "Calling buildConfig(`n>>> `$configUrl=$configUrl`n>>> )" "darkgray"  
+
     $downloadPath = "c:\push\wksMaintenanceConfig.zip"
     $csvPath = "c:\push\wksMaintenanceConfig.csv"
     $lod = @{}
@@ -55,38 +64,171 @@ function buildConfig($configUrl){
 
     return $lod
 }
-function deleteDirContents ($dir){
-    log " Calling deleteDirContents(`n>>> `$dir=$dir`n>>> )" "darkgray"
-    $ndirName = $dir + "_OLD"
-    #rename C:\Windows\SoftwareDistribution\Download to OLD
-    log "...renaming [$dir] to [$ndirName]." "gray"
-    Rename-Item -Path $dir -NewName $ndirName
-    log ">> (dir $ndirName).Length == $((dir $dir).Length)" "darkgray"
-    #create a new blank dir
-    log "...creating a new dir [$dir]." "gray"
-    New-Item -Path $dir -ItemType Directory
-    log ">> (dir $dir).Length == $((dir $dir).Length)" "darkgray"
-    #delete renamed dir    
-    log "...deleting dir [$ndirName]." "gray"
-    Remove-Item $ndirName -Force -Recurse
-    #verify dir is now empty
-    
-    log "...verifiying that deleting dir is empty [dir ($dir).Length -lt 1]." "gray"
-    $res = (((dir $dir).Length) -lt 1)
-    if ($res){
-        log "...Successfully deleted contents of [$dir]." "green"
-    }else{
-        log "...Failed to delete contents of [$dir]." "red"        
+function getStorageSize($drive=$null){
+    log "Calling getStorageSize(`n>>> `$drive=$drive`n>>> )" "darkgray"  
+    $data = get-wmiobject win32_LogicalDisk -Filter "DriveType = 3" | 
+        Select DeviceID, FreeSpace, Size
+    $data | foreach {
+        Add-Member -InputObject $_ -NotePropertyName "UsedSpace" -NotePropertyValue ($_.Size - $_.FreeSpace)
     }
-
-    return $res  
+    if ($drive -eq $null) {
+        $sum = sum $data.Size
+        return $sum/1Gb
+    }else{
+        $data | foreach {
+            if($_.DeviceID -contains ($drive + ":")){
+                return $_.Size/1Gb
+            }
+        }
+    }
 }
+function deleteFile($path){
+    log " Calling deleteFile (`n>>> `$path=$path`n>>> )" "darkgray" 
+    try{
+        remove-item -Path $path -Recurse -Force -EA STOP
+    }catch [System.IO.IOException]{
+        log ">>WARNING: $($ERROR[4].Exception.Message)" "Yellow"
+        log ">>   Could not delete [$($error[5].TargetObject)]." "darkgray"
+
+        return $false
+    } catch [System.Management.Automation.RuntimeException] {
+        log ">> Warning: $($error[0].exception.message)..." "Yellow"
+        log "Could not delete [$($error[5].TargetObject)]." "darkgray"
+        return $false
+    } catch {
+        log "E!"
+        log ">> UNCAUGHT ERROR: $PSItem" "red"
+        log ">> UNCAUGHT ERROR: $($Error[0].Exception.GetType().fullname)" "red"
+        return $false
+    }
+}
+function deleteDirContents ($dir){
+    log " Calling deleteDirContents(`n>>> `$dir=$dir`n>>> )" "darkgray"  
+
+    if(checkToSeeIfDirIsEmpty $dir){
+        log "dir [$dir] is already empty.  Skipping" "gray"
+        $res = $true
+
+    }else{
+        try {
+            #del contents of $dir
+            log "deleting contents of [$dir]." "gray"
+            $files = (dir $dir -Recurse -File | select FullName).FullName 
+        
+            if($files.Length -lt 1){
+                log "dir [$dir] is empty.  Skipping." "gray"
+                $res = $true
+            }else{
+                $files | foreach {
+                    deleteFile $_
+                }
+                log "(dir $dir).Length == $((dir $dir).Length)" "darkgray"
+                log "(dir $ndirName).Length == $((dir $ndirName).Length)" "darkgray"
+            }
+        
+ 
+            #verify dir is now empty
+            $res = checkToSeeIfDirIsEmpty $dir  
+
+        } catch [System.Management.Automation.RuntimeException] {
+            log ">> CAUGHT ERROR: $($error[0].exception)..." "Yellow"
+            log ">> Could not delete [$($error[5].TargetObject)]." "Yellow"
+            return $false
+        } catch {
+            log "E!"
+            log ">> UNCAUGHT ERROR: $PSItem" "red"
+            log ">> UNCAUGHT ERROR: $($Error[0].Exception.GetType().fullname)" "red"
+            return $false
+        }
+    }
+    return $res
+}
+function moveAndDeleteDirContents ($dir){
+    log " Calling moveAndDeleteDirContents(`n>>> `$dir=$dir`n>>> )" "darkgray"  
+    
+    #rename C:\Windows\SoftwareDistribution\Download to OLD
+    #log "...renaming [$dir] to [$ndirName]." "gray"
+    #$t = Rename-Item -Path $dir -NewName $ndirName
+    #create a new temp dir
+    $ndirName = $dir + "_OLD"
+    $n = 1
+    while (test-path $ndirName){
+        $ndirName = $ndirName + "_$n"
+        $n+=1
+    }
+    log "...creating a new temp dir [$ndirName]." "gray"
+    $t = New-Item -Path $ndirName -ItemType Directory
+    log "$t" "darkgray"
+
+    try {
+        #move contents of $dir to $ndirName
+        log "...moving contents of [$dir] to [$ndirName]." "gray"
+        Move-Item -Path $dir\* -Destination $ndirName -Force -EA SilentlyContinue
+        log "(dir $dir).Length == $((dir $dir).Length)" "darkgray"
+        log "(dir $ndirName).Length == $((dir $ndirName).Length)" "darkgray"
+    
+        #delete contents of ndirName    
+        log "deleting dir [$ndirName]." "gray"
+        $t = Remove-Item $ndirName -Force -Recurse
+        log "$t" "darkgray"
+ 
+        #verify dir is now empty
+        $res = checkToSeeIfDirIsEmpty $dir
+        return $res  
+
+    } catch [System.Management.Automation.RuntimeException] {
+        log ">> CAUGHT ERROR: $($ERROR[0].exception)..." "Yellow"
+        log ">> Could not delete [$($ERROR[0].TargetObject)]." "Yellow"
+        return $false
+    } catch {
+        log "E!"
+        log ">> UNCAUGHT ERROR: $PSItem" "red"
+        log ">> UNCAUGHT ERROR: $($Error[0].Exception.GetType().fullname)" "red"
+        return $false
+    }
+}
+
+function checkToSeeIfDirIsEmpty ($pathToDir){
+    log "Calling checkToSeeIfDirIsEmpty(`n>>> `$pathToDir=$pathToDir`n>>> )" "darkgray"  
+    log "verifiying that dir [$pathToDir] is empty." "gray"
+    log "dir ($pathToDir).Length -lt 1" "darkgray"
+    try { 
+        if (Test-path $pathToDir){  
+            $res = (((dir $pathToDir -ErrorAction Stop).Length) -lt 1) 
+        }else{
+            log "Dir [$pathToDir] does not exist.  Skipping." "gray"
+            $res = $true
+        }
+
+        $remainingFiles = (dir $pathToDir -ErrorAction Stop).Length
+
+        if ($res){
+            log "Successfully deleted all contents of [$pathToDir]." "green"
+        }elseif ($remainingFiles -lt 10){
+            log "Successfully deleted almost all contents of [$pathToDir].  $remainingFiles remaining." "green"
+        }else{
+            log "Failed to delete contents of [$pathToDir].  $remainingFiles remaining" "red"        
+        }
+        return $res   
+    } catch [System.Management.Automation.ItemNotFoundException] {
+        log ">> WARNING: $($ERROR[0].Exception.Message)" "Yellow"
+        return $true
+    } catch {
+        log "E!"
+        log ">> UNCAUGHT ERROR: $PSItem" "red"
+        log ">> UNCAUGHT ERROR: $($Error[0].Exception.GetType().fullname)" "red"
+        return $false
+    }
+}
+
 function deleteTempFiles (){
+    log "Calling deleteTempFiles(`n>>> no args`n>>> )" "darkgray"
     $tdirs = @(
         "c:\temp",
-        "c:\windows\temp"
-        ) #how to add %user%\appdata\temp for all users?
-
+        “C:\Windows\Temp",
+        “C:\Windows\Prefetch",
+        “C:\Documents and Settings\Local Settings\temp”
+        )
     #delete the files from $tdirs recursively
     $res = @()
     $tdirs | foreach {
@@ -96,47 +238,141 @@ function deleteTempFiles (){
             "res"=$r;
         }
     }
+    return !($res.res).contains($false)
+}
+function checkTempDirs (){
+    log "Calling checkTempDirs(`n>>> no args`n>>> )" "darkgray"
+    $tdirs = @(
+        "c:\temp",
+        “C:\Windows\Temp",
+        “C:\Windows\Prefetch",
+        “C:\Documents and Settings\Local Settings\temp”
+        )
+    #check to see if  the files from $tdirs recursively
+    $res = @()
+    $tdirs | foreach {
+        $r = checkToSeeIfDirIsEmpty $_
+        $res += @{
+            "dir"=$_;
+            "res"=$r;
+        }
+    }
+    return !($res.res).contains($false)
+}
+
+function deleteUserTempFiles(){
+    log "Calling deleteUserTempFiles(`n>>> no args`n>>> )" "darkgray"
+    try{
+        foreach ($u in (get-item c:\users\*).Name) {
+            log "checking temp files for [$u]." "gray"
+            $tds = (Get-ChildItem ("C:\Users\" +$u+ "\AppData\Local\Temp\") -Recurse -ErrorAction SilentlyContinue | 
+                ?{ $_.PSIsContainer } |
+                select FullName).FullName
+            $tds += "C:\Users\" +$u+ "\AppData\Local\Temp\"
+            if ($tds.Length -gt 0){
+                $tds | foreach {
+                    log "deleting contents of [$_]." "gray"
+                    try{
+                        Remove-Item $_ -Recurse -Force -ErrorAction Continue
+                    } catch {
+                        log "E!"
+                        log ">> UNCAUGHT ERROR: $PSItem" "red"
+                        log ">> UNCAUGHT ERROR: $($Error[0].Exception.GetType().fullname)" "red"
+                    }
+                }
+            }else{
+                log "no files to delete for [$u]" "gray"
+            }
+        }
+        $res = $true
+    } catch [System.UnauthorizedAccessException] {
+        log ">> Skipping [$($ERROR[0].TargetObject)]. $($ERROR[0].Exception)" "yellow"
+        return $false
+    } catch {
+        log "E!"
+        log ">> UNCAUGHT ERROR: $PSItem" "red"
+        log ">> UNCAUGHT ERROR: $($Error[0].Exception.GetType().fullname)" "red"
+        return $false
+        }
     return $res
 }
 function deleteOldUpdateFiles(){
-    log " Calling deleteOldUpdateFiles(`n>>> no args`n>>> )" "darkgray"
+    log "Calling deleteOldUpdateFiles(`n>>> no args`n>>> )" "darkgray"
     $ufDir = "C:\Windows\SoftwareDistribution"
-    log ">> pre function (dir $dir).Length == $((dir $dir).Length)" "darkgray"
+    log "pre function (dir $ufDir).Length == $((dir $ufDir).Length)" "darkgray"
     #Stop Windows Update Service (wauaserv)
-    Stop-Service wuauserv
+    $wus = "wuauserv"
+    $n = 0
+    while(((get-service $wus).Status -ne "Stopped") -and ($n -lt 5)){
+        Stop-Service $wus
+        $n+=1
+        timeout /t 5
+        if($n -gt 5){
+            log "...timed out while waiting for svc to close." "red"
+            return $false
+            break
+        }
+    }
+    log "Service State [$((get-service $wus).DisplayName)]: [$((get-service $wus).Status)]" "darkgray"
     #delete update dir and replace with a blank dir
     $res = deleteDirContents $ufDir
     #Start Windows Update Service (wauaserv)
-    Start-Service wuauserv
-    log ">> post function (dir $dir).Length == $((dir $dir).Length)" "darkgray"
+    Start-Service $wus
+    log "Service State [$((get-service $wus).DisplayName)]: [$((get-service $wus).Status)]" "darkgray"
+    log "post function (dir $ufDir).Length == $((dir $ufDir).Length)" "darkgray"
     return $res    
 }
 function emptyRecyclingBin(){
-    Clear-RecycleBin #doesn't work on psv2, works in v5
+    log "Calling emptyRecyclingBin(`n>>> no args`n>>> )" "darkgray"
+    #doesn't work on psv2, works in v5
+    Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 }
 function flushDNS() {
-
+    log "Calling flushDNS(`n>>> no args`n>>> )" "darkgray"
+    Clear-DnsClientCache
 }
 function runDiskCleanup(){
-
+    log "Calling runDiskCleanup(`n>>> no args`n>>> )" "darkgray"
+    $t = cleanmgr /verylowdisk
+    $n = 0
+    While($true) {
+        timeout /t 10
+        if(checkTempDirs){
+            log "...completed disk cleanup." "gray"            
+        }
+        if($n -gt 5){
+            log "WARNING: Timed out while waiting for Dick Cleanup." "Yellow"
+            break
+        }
+        $n += 1
+    }
 }
-funcion runDiskCheck(){
-
+function runDiskCheck(){
+    log "Calling runDiskCheck(`n>>> no args`n>>> )" "darkgray"
+    $t = CHKDSK /B /X
 }
 function defragDisk($disk){
-
+    log "Calling defragDisk(`n>>> `$disk=$disk`n>>> )" "darkgray"
+    pass
 }
 function main(){
-    buildConfig
-
+    #buildConfig
+    $pre = getStorageSize
+    
     deleteTempFiles
+    #deleteUserTempFiles
     deleteOldUpdateFiles
     emptyRecyclingBin
     flushDNS
     runDiskCleanup
-    runDiskCheck
-    defragDisk
+    checkTempDirs
+    $post = getStorageSize
+    log "Removed $($pre-$post) Gb of files." "green"
+
+    #runDiskCheck #schedules a diskcheck for the next reboot
+    #Restart-Computer    
 }
 
 #may want to upgrade PS first.
-main $configCsv
+clear
+main
