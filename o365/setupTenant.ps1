2eabe24c91e68eb9876d3fa3f5de3e947adb9f51
Luke <cliftonlboyd@gmail.com>
2018-09-19 08:51:54 -0400

    Update setupTenant.ps1

--- o365/setupTenant.ps1
+++ o365/setupTenant.ps1
@@ -5,7 +5,7 @@
     $licenseUri = $rmsConfig.LicensingIntranetDistributionPointUrl
 }
 
-function connectEx($uc){
+function connectEx($UserCredential){
     $ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
     Import-PSSession $ExchangeSession
     Import-Module MsOnline; 
